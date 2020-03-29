374bb9aa5d06d67861ff28da1a7496f916dacdbc
Olga Chaiko <chaiko.olga@gmail.com>
2020-03-29 22:50:52 +0300

    add changes tasks 4 and 5

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,9 +2,52 @@ package subtask4
 
 class StringParser {
 
-
-//    fun getResult(inputString: String): Array<String> {
-//
-//        throw NotImplementedError("Not implemented")
-//    }
+    fun getResult(inputString: String): Array<String> {
+        val wordArray = mutableListOf<String>()
+        for (i in inputString.indices) {
+            val word = when (inputString[i]) {
+                '<' -> addWord(inputString.substring(i), 1)
+                '(' -> addWord(inputString.substring(i), 2)
+                '[' -> addWord(inputString.substring(i), 3)
+                else -> ""
+            }
+            if (word.length != 0 || word != "") {
+                wordArray.add(word)
+            }
+        }
+        return wordArray.toTypedArray()
+    }
 }
+
+    fun addWord(subString: String, number: Int): String {
+        val firstChar = when (number) {
+            1 -> '<'
+            2 -> '('
+            3 -> '['
+            else -> ""
+    }
+        val finalChar = when (number) {
+            1 -> '>'
+            2 -> ')'
+            3 -> ']'
+            else -> ""
+    }
+
+        var word = ""
+        var check = 0
+
+        for (i in 1 until subString.length) {
+            if (subString[i] == firstChar)
+                check = check + 1
+            else if (subString[i] ==
+                finalChar && check != 0
+        )
+                check = check - 1
+            else if (subString[i] == finalChar && check == 0) {
+                word = subString.substring(1, i)
+            break
+        }
+    }
+    return word
+}
+
--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,23 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val array = digitString.toCharArray()
+        var count = 0
+        for (index in array.indices) {
+            if (index > array.size / 2) break
+
+            if (array[index] != array[array.size - index - 1]) {
+                if (array[index] == '0') {
+                    array[index] = '9'
+                }
+                array[array.size - index - 1] = array[index]
+                count++
+            } else if (count > k) {
+                return "-1"
+            }
+        }
+        return String(array)
     }
 }
